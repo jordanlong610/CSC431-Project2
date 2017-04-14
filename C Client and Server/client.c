29362ebb8d6edf1e3f2072485cf79037071e799b
kshoop250 <kshoop250@gmail.com>
2017-04-14 02:15:42 -0400

    newest upload  2:14 AM, 14 April 2017
    had issues with segmentation faults which are now fixed. The two programs run fine, but when the client sends the message to the router, the router does not accept the connection even though the client says the send was successful...

--- C Client and Server/client.c
+++ C Client and Server/client.c
@@ -3,51 +3,128 @@ Programmer: Kyle Shoop
 Professor: Dr. Girard
 Class: Computer networks
 file: client.c
-Date: 1 Feb. 2017
+Date: 4 April 2017
 */
 
 #include <sys/types.h>
 #include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <stdio.h>
+#include <signal.h>
 #include <unistd.h>
 #include <netdb.h>
+
+
+
+///start here...... need to make client able to recieve messages from router
+
+
+
+
+
+
+uint8_t checksum ( uint8_t array[5])
+{
+        uint8_t sum = 0x00;
+        int temp;
+
+        for(temp = 0; temp < 5; temp ++)
+        {
+		if(temp != 2)
+                	sum = sum + array[temp];
+        }
+
+        return sum;
+}
+
+
+
 
 int main()
 {
 
-	int sockfd, m_count = 1;
-	char sendline[100];
-	char recvline[100];
-	struct sockaddr_in servaddr;
+	int sockfd, m_count, exit = 0;
+	
+	uint8_t data = 0x01;
+
+	uint8_t sendline[5];
+	uint8_t recvline[5];
+	struct sockaddr_in routeraddr;
+
+	int client_num = 0;
+
+	printf("\nPlease enter client number to initialize: ");
+	scanf("%d", &client_num);
+	printf("\n\n");
+
+
+	signal(SIGPIPE, SIG_IGN);
 
 	sockfd = socket (AF_INET, SOCK_STREAM, 0);
-	bzero (&servaddr, sizeof (servaddr));	
+	bzero (&routeraddr, sizeof (routeraddr));	
 
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(4446);
+	routeraddr.sin_family = AF_INET;
+	routeraddr.sin_port = htons(4446);
 
-	inet_pton(AF_INET,"127.0.0.1", &(servaddr.sin_addr));
+	inet_pton(AF_INET,"127.0.0.1", &(routeraddr.sin_addr));
 
-	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	connect(sockfd, (struct sockaddr *)&routeraddr, sizeof(routeraddr));
 
-	while(1)
-	{
+		bzero(sendline, 5);
+		bzero(recvline, 5);
 
-		bzero(sendline, 100);
-		bzero(recvline, 100);
+		//byte 1, MESSAGE SOURCE
+		sendline[0] = (uint8_t) client_num;
 
-		printf("\n(client) input %d: ", m_count);
+		//byte 2, MESSAGE DESTINATION
+		sendline[1] = 0x01;
 
-		fgets(sendline, 100, stdin);
+		//byte 4, DATA byte 1
+		sendline[3] = data;
 
-		write(sockfd, sendline, strlen(sendline)+1);
-		read(sockfd, recvline, 100);
-		printf("\n(client) output from server: %s", recvline);
-		m_count++;
+		//byte 5, DATA byte 2
+		sendline[4] = data;
 
-	}	
+		//byte 3, 8-bit CHECKSUM
+    sendline[2] = checksum(sendline);
+
+		
+		for(m_count = 0 ; m_count < 5; m_count ++)
+		{
+			printf("(client) byte %d = " "%" PRIu8 "\n" ,m_count, sendline[m_count]);
+		}
+		printf("\n");
+
+		exit = write(sockfd, sendline, sizeof(sendline)+1);
+
+		if(exit < 0)
+		{
+			printf("\n(client) ERROR, unable to write to router.\n*Program Terminated*\n\n");
+			return 0;
+		}
+     else
+     {
+       printf("write successful.\n\n");
+     }
+    /*
+		exit = read(sockfd, recvline, sizeof(recvline)+1);
+
+                if(exit < 0)
+                {
+                        printf("\n(client) ERROR, unable to read from router.\n*Program Terminated*\n\n");
+                        break;
+                }
+
+
+		 for(m_count = 0 ; m_count < 5; m_count ++)
+                {
+			printf("\n(client) output from server: " "%" PRIu8 "\n", recvline[m_count]);
+		}
+    */
+    
 	return 0;
 
 }
--- C Client and Server/pserver2.c
+++ C Client and Server/pserver2.c
@@ -2,12 +2,13 @@
 Programmer: Kyle Shoop
 Professor: Dr. Girard
 Class: Computer networks
-file: basic.c 
-Date: 15 February 2017
+file: pserver2.c 
+Date: 4 April 2017
 */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <inttypes.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netdb.h>
@@ -15,50 +16,181 @@ Date: 15 February 2017
 #include <string.h>
 #include <sys/types.h>
 #include <pthread.h>
+#include <stdint.h>
+#include <arpa/inet.h>
 
-#define NUM_THREADS 10
-
-void* pthread(void* socket)
-{
-	long comm_socket, m_count;
-		
-	for(m_count = 0; m_count < 10; m_count++)
-	{
-
-		char str[100];
-		comm_socket = (long) socket;
-		
-		int location, shift, length;
-
-		bzero(str, 100);
-
-		read(comm_socket, str, 100);
-
-		if(str[strlen(str)-1] == '\n')
-			length = strlen(str) -1;
-		else
-			length = strlen(str);
-
-		printf("\n(server)reversing - %s", str);
-
-		for(location = 0; location < length; location++)
-		{
-
-			for(shift = 0; shift < length - (location+1); shift++)
-			{
+//change IP addresses depending on connections
+#define CLIENT_IP "127.0.0.1"
+#define ROUTER1_IP "127.0.0.1"
+#define ROUTER2_IP "127.0.0.1"
+#define ROUTER3_IP "127.0.0.1"
+#define ROUTER4_IP "127.0.0.1"
 			
-				char temp;
-				
-				temp = str[shift];
-				str[shift] = str[shift +1];
-				str[shift+1] = temp;
+
+//structure for passing data to threads
+struct data{
+	long socket;
+	int server_number;
+} to_thread, *thread_data;
+
+uint8_t message[5];
+
+struct lookup{
+	int neighbor1_num, neighbor2_num;
+	char neighbor1_ip, neighbor2_ip;
+}table;
+
+//checksum function
+uint8_t checksum ( uint8_t array[5])
+{
+        uint8_t sum = 0x00;
+        int temp;
+
+        for(temp = 0; temp < 5; temp ++)
+        {
+                if(temp != 2)
+                        sum = sum + array[temp];
+        }
+
+        return sum;
+}
+
+void* send_func(void* argp)
+{
+
+	char send_ip[9];
+  int sockfd;
+  int exit;
+  struct sockaddr_in addr;
+
+	//check to determine where to send the received message
+	//		
+	//
+	//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+	if (thread_data->server_number == message[1])
+	{
+		//send_ip = CLIENT_IP;
+    strcpy(send_ip, CLIENT_IP);
+		
+	}
+	else 
+	{
+		if(message[1] == table.neighbor1_num)
+		{
+			//send_ip = table.neighbor1_ip;
+      strcpy(send_ip, &table.neighbor1_ip);
+		}
+		else if(message[1] == table.neighbor2_num)
+		{
+			//send_ip = table.neighbor2_ip;
+      strcpy(send_ip, &table.neighbor2_ip);
+		}
+		else
+		{
+			//send_ip = table.neighbor1_ip;
+      strcpy(send_ip, &table.neighbor1_ip); 
+		}
+	}
+
+	//send message
+	sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	bzero (&addr, sizeof (addr));	
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(4446);
+
+	inet_pton(AF_INET, send_ip, &(addr.sin_addr));
+
+	connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+
+	printf("connected to client\n\n");
+
+	exit = write(sockfd, message, sizeof(message)+1);
+
+	if(exit < 0 ) 
+	{
+		printf("(router) Send to client failed.");
+	}
+
+	pthread_exit(NULL);
+}
+
+void* receive(void* argp)
+{
+	//set thread_data structure pointer equal to void* pointer passed to thread
+	thread_data = (struct data *) argp;
+
+	printf("Router %d Created\n\n", thread_data->server_number);
+
+		long connection_socket;		
+		
+		int temp, exit = 0;
+		
+
+		//set variable to socket connection		
+		 connection_socket = thread_data->socket;
+
+
+		//zero the message variable
+		bzero(message, sizeof(message));
+	
+		//read message from client
+		exit = read(connection_socket, message, sizeof(message)+1);
+
+		//check for read error
+		if( exit < 0 )
+		{
+			printf("\n(router) ERROR, unable to read from client.\n*Thread Terminated*\n\n");
+			pthread_exit(NULL);
+		}
+
+		//perform checksum error detection
+		if(message[2] == checksum(message))
+		{
+			//message must have a source, if not there was an error
+			if(message[0] == 0)
+			{
+				printf("\n(router) ERROR, unable to read from client.\n*Thread Terminated*\n\n");
+				pthread_exit(NULL);
+			}
+			
+
+			//print the message received by the router
+			for(temp = 0; temp < 5; temp ++)
+               		{
+                        	printf("(router) message received byte %d = " "%" PRIu8 "\n", temp, message[temp]);
+               		}
+
+			
+					
+			//send message to client
+			int error = 0;
+			pthread_t send_thread;
+
+      printf("creating send thread");
+			error = pthread_create(&send_thread,NULL, send_func, (void *)&to_thread);
+		
+			
+			if(error)
+			{
+
+				printf("ERROR #%d, program terminated", error);
+				pthread_exit(NULL);
 
 			}
-		}
-		write(comm_socket, str, strlen(str)+1);
 
-	}
-	
+			//wait for send thread to finish and close
+			pthread_join(send_thread,NULL);
+			printf("\nSend thread exited successfully\n");
+								
+		}
+		//checksum failed
+		else
+		{
+			printf("\n(router) ERROR, Checksum failed\n message ignored\n\n");
+		
+		}
 
 	pthread_exit(NULL);
 }
@@ -68,33 +200,80 @@ void* pthread(void* socket)
 int main()
 {
 
-	pthread_t threads[NUM_THREADS];	
+	pthread_t receive_thread;	
 
-	long t_num, error;
+	long error;
+
+	int server_num;
+
+	int send_flag;
 	
+	signal(SIGPIPE, SIG_IGN);
+
 	long listen_socket, comm_socket;
 
-	struct sockaddr_in server_info;
+	struct sockaddr_in router_info;
+
+	printf("\nPlease enter router number to initialize (must be same as connecting client): ");
+	scanf("%d", &server_num);
+	printf("\n\n");
+ 
+	//save server number
+	to_thread.server_number = server_num;
+
+	//build lookup table with neighboring router numbers
+	switch(server_num){
+		case 1: 
+			table.neighbor1_num = 2;
+			table.neighbor2_num = 4;
+			table.neighbor1_ip = *ROUTER2_IP;
+			table.neighbor2_ip = *ROUTER4_IP;
+			break;
+		case 2:
+			table.neighbor1_num = 1;
+			table.neighbor2_num = 3;
+			table.neighbor1_ip = *ROUTER1_IP;
+			table.neighbor2_ip = *ROUTER3_IP;
+			break;
+		case 3: 
+			table.neighbor1_num = 2;
+			table.neighbor2_num = 4;
+			table.neighbor1_ip = *ROUTER2_IP;
+			table.neighbor2_ip = *ROUTER4_IP;
+			break;
+		case 4:
+			table.neighbor1_num = 1;
+			table.neighbor2_num = 3;
+			table.neighbor1_ip = *ROUTER1_IP;
+			table.neighbor2_ip = *ROUTER3_IP;
+			break;
+	}
 
 	listen_socket = socket (AF_INET, SOCK_STREAM, 0);
 
-	bzero(&server_info, sizeof(server_info));
+	bzero(&router_info, sizeof(router_info));
 
-	server_info.sin_family = AF_INET;
-	server_info.sin_addr.s_addr = htons(INADDR_ANY);
-	server_info.sin_port = htons(4446);
+	router_info.sin_family = AF_INET;
+	router_info.sin_addr.s_addr = htons(INADDR_ANY);
+	router_info.sin_port = htons(4446);
 
-	bind(listen_socket,(struct sockaddr*)&server_info, sizeof(server_info));
+	bind(listen_socket,(struct sockaddr*)&router_info, sizeof(router_info));
 
+  printf("router waiting for connection...\n\n");
+  
+  //listen for connections
 	listen(listen_socket, 10);
-
-	for(t_num = 0; t_num < NUM_THREADS; t_num++)
-	{
+	
+	while(1)
+	{	
+		//create thread for receive socket accept
 		comm_socket = accept(listen_socket, (struct sockaddr*) NULL, NULL);
 
-		printf("\ncreating thread #%ld\n",t_num);
+		to_thread.socket = comm_socket;
 
-		error = pthread_create(&threads[t_num],NULL, pthread, (void*)comm_socket);
+		printf("\ncreating receive thread\n");
+
+		error = pthread_create(&receive_thread,NULL, receive, (void *)&to_thread);
 		
 		if(error)
 		{
@@ -104,15 +283,12 @@ int main()
 
 		}
 
-	}
-
-	for(t_num = 0; t_num < NUM_THREADS; t_num++)
-	{
-
-		pthread_join(threads[t_num],NULL);
-		printf("\nthread #%ld exited successfully\n", t_num);
+		//wait for receive thread to finish and close
+		pthread_join(receive_thread,NULL);
+		printf("\nReceive thread exited successfully\n");
 
 	}
+	
 
 
 	pthread_exit(NULL);
