633339a1cdf17fe1c62233c1ef5934473f3f0e39
cl9710 <cl9710@cs.ship.edu>
2017-04-15 01:11:32 -0400

    Modified client to act as server (doesnt work though). Client uses clock instead of sleeping for determing when to send messages now. Filled out router methods (routing table and forwarding method, but still needs work).

--- src/Client.java
+++ src/Client.java
@@ -3,80 +3,90 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.lang.ClassNotFoundException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * A simple client that connects to a server on port 4446 and sends a message.
+ * This client application will ask for a 10 character input, which will be reversed
+ * by the server and send back.
+ *
+ * @author Jordan Long, Chris Lashley, Jeff Titanich, Kyle Shoop
+ */
 public class Client
 {
-	/**
-	 * A simple client that connects to a server on port 4446 and sends a message.
-	 * This client application will ask for a 10 character input, which will be reversed
-	 * by the server and send back.
-	 *
-	 * @author Jordan Long, Chris Lashley, Jeff Titanich, Kyle Shoop
-	 *
-	 */
 
-	static int SOURCE = randomDestination();  //Static Client ID
-	static int PORT = 4446;
+	//	static int SOURCE = randomDestination();  // testing purposes
+	static int SOURCE = 1;  //Static Client ID
+	static int CPORT = 4446;	// client port for sending msgs out
+	static int SPORT = 4447;	// server port for receiving msgs
 
 	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException
 	{
-            /**
-             * Create connection to server.
-             * Uses localhost:4446 as the default settings
-             * Initialize Scanner for user input.
-             */
-            InetAddress host = InetAddress.getLocalHost();
-        	Socket socket = new Socket(host.getHostName(), 4446);
+		/**
+		 * Create connection to server.
+		 * Uses localhost:4446 as the default settings
+		 * Initialize Scanner for user input.
+		 */
+		InetAddress host = InetAddress.getLocalHost();
+		Socket socket = new Socket(host.getHostName(), CPORT);
 
-            /**
-             * Creates a conditional loop where the client can only
-             * send a max of 10 messages before terminating.
-             */
+		/**
+		 * Creates a conditional loop where the client can only
+		 * send a max of 10 messages before terminating.
+		 */
+		int data1 = 1;
+		int data2 = 1;
+		long timeLastMsg = System.currentTimeMillis();
 
-            int data1=1;
-    		int data2=1;
-            while (true)
-            {
+		/**
+		 * A server is created listening for msgs on port 4447. Each time a new
+		 * msg comes, a new thread is created as the router sends it to the client
+		 * Send msg out to random client via router if last msg was sent over 2s ago.
+		 * increment data value each time a msg is sent
+		 */
+		@SuppressWarnings("resource") //Server is never closed
+		ServerSocket server = new ServerSocket(SPORT);
+		//		Router r = new Router();
+		//		Client c = new Client();
+		while (true)
+		{
+			// checks to see if 2 seconds has passed, if so, sends a new message to random client
+			long currentTime = System.currentTimeMillis();
+			long timeDif = currentTime - timeLastMsg;
+			if (timeDif > 2000)
+			{
+				sendMessage(SOURCE, randomDestination(), data1, data2, socket);
+				timeLastMsg = currentTime;
+				data1++;
+				data2++;
 
-                /**
-                 * Send stored String to server.
-                 * Receive Message.
-                 */
+				//				receiveMessage(socket);
+			}
 
-                sendMessage(SOURCE, randomDestination(), data1, data2, socket);
+			// listen for msgs and let msg handler create a new thread to handle incoming msgs
+			Socket ssocket = server.accept();
+			//        	MessageHandler mh  = new MessageHandler(socket, r);
+			//        	MessageHandler mh  = new MessageHandler(socket, c);
+			MessageHandler mh  = new MessageHandler(socket);
+			mh.start();
 
+			/**
+			 * Receive any messages from Router
+			 */
+			//			receiveMessage(socket);
+		}
 
-
-                /**
-                 * Receive Message from Router
-                 */
-                byte[] receiveMessage = receiveMessage(socket);
-
-
-
-                /**
-                 * Increase data by 1, sleep 2 seconds
-                 */
-                data1++;
-                data2++;
-                Thread.sleep(2000);
-            }
-
-            /**
-             * Close all streams and finally the socket.
-             */
-            //socket.close();
-            //userInput.close();
+		/**
+		 * Close all streams and finally the socket.
+		 */
+		//socket.close();
+		//userInput.close();
 	}
-
-
-
 
 	/**
 	 * Sends message to a router. Creates a byte array with the 5 bytes of data.
-	 *
 	 * @param source Source Client ID
 	 * @param destination Destination Client ID
 	 * @param data1 Data packet to be sent
@@ -86,7 +96,7 @@ public class Client
 	 */
 	public static void sendMessage(int source, int destination, int data1, int data2, Socket socket) throws IOException
 	{
-        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
 		byte[] message = {(byte) (source), (byte) (destination), 0, (byte) (data1), (byte) (data2)};
 		message[2] = checksum(message);		//Calculate checksum and attach to message.
@@ -94,10 +104,11 @@ public class Client
 		System.out.println("Client sent message to Destination: " + message[1]);
 		System.out.println("Data1: " + message[3]);
 		System.out.println("Data2: " + message[4]);
+		System.out.println("");
 
 		output.write(message);
-        output.flush();
-}
+		output.flush();
+	}
 
 	/**
 	 * Receives a message from the router.
@@ -108,29 +119,27 @@ public class Client
 	 * @return receiveMessage The message from the router.
 	 * @throws IOException
 	 */
-	public static byte[] receiveMessage(Socket socket) throws IOException
+	public static void receiveMessage(Socket socket) throws IOException
 	{
-        DataInputStream input = new DataInputStream(socket.getInputStream());
-		byte[] message= new byte[5];
+		DataInputStream input = new DataInputStream(socket.getInputStream());
+		byte[] message = new byte[5];
 
 		input.readFully(message, 0, 5);
 
-        byte calculateChecksum = checksum(message);
-        if(calculateChecksum == message[2])
-        {
-        	System.out.println("Client Received Message from Source: " + message[0]);
-        	System.out.println("Checksum valid, message is not corrupt.");
-    		System.out.println("Data1: " + message[3]);
-    		System.out.println("Data2: " + message[4]);
-    		System.out.println("");
-        }
-        else
-        {
-        	System.out.println("Checksum not vaild, message may be corrupt.");
-        }
-
-		return message;
-}
+		byte calculateChecksum = checksum(message);
+		if(calculateChecksum == message[2])
+		{
+			System.out.println("Client Received Message from Source: " + message[0]);
+			System.out.println("Checksum valid, message is not corrupt.");
+			System.out.println("Data1: " + message[3]);
+			System.out.println("Data2: " + message[4]);
+			System.out.println("");
+		}
+		else
+		{
+			System.out.println("Checksum not vaild, message may be corrupt.");
+		}
+	}
 
 	/**
 	 * Calculates checksum of received message. Returns the value of the checksum.
@@ -149,8 +158,100 @@ public class Client
 	 */
 	public static byte randomDestination()
 	{
-		byte destination = (byte) (Math.random() * 4);
+		byte destination = (byte) (Math.random() * 4 + 1);
+		// clients shouldnt send to themselves
+		if (destination == SOURCE)
+			while (destination == SOURCE)
+				destination = (byte) (Math.random() * 4 + 1);
 		return destination;
 	}
+}
 
+/**
+ * class for receiving msgs from the router sent from other clients
+ * @author Chris
+ */
+class MessageHandler extends Thread
+{
+	private Socket socket;
+	//    private Router router;
+	//    private Client client;
+	DataInputStream input;
+	DataOutputStream output;
+	//    public MessageHandler(Socket socket, Router r) throws IOException
+	//    public MessageHandler(Socket socket, Client c) throws IOException
+	public MessageHandler(Socket socket) throws IOException
+	{
+		this.socket = socket;
+		//        this.router = r;
+		//        this.client = c;
+		input = new DataInputStream(socket.getInputStream());
+		output = new DataOutputStream(socket.getOutputStream());
+
+	}
+
+
+	/**
+	 * Here is where we read in a packet and then close the socket.
+	 */
+	public void run()
+	{
+		while (socket.isConnected() && !socket.isClosed())
+		{
+			try
+			{
+				receiveMessage();
+				input.close();
+				output.close();
+				socket.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+	/**
+	 * Calculates checksum of received message. Returns the value of the checksum.
+	 * @param data Byte array of received message.
+	 * @return The computed checksum value
+	 */
+	public static byte checksum(byte[] data)
+	{
+		int checksum = 256 - (data[0]+data[1]+data[3]+data[4]);
+		return (byte)(checksum);
+	}
+
+
+
+	/**
+	 *  Read message from router via another client. Prints to console for reference.
+	 */
+
+	public void receiveMessage()
+	{
+		byte[] message = new byte[5];
+		try
+		{
+			input.readFully(message, 0, 5);
+		}
+		catch (IOException e1)
+		{
+			e1.printStackTrace();
+		}
+		byte calculateChecksum = checksum(message);
+		if(calculateChecksum == message[2])
+		{
+			System.out.println("Client Received Message from Source: " + message[0]);
+			System.out.println("Checksum valid, message is not corrupt.");
+			System.out.println("Data1: " + message[3]);
+			System.out.println("Data2: " + message[4]);
+		}
+		else
+		{
+			System.out.println("Checksum not vaild, message may be corrupt.");
+		}
+	}
 }
--- src/Router.java
+++ src/Router.java
@@ -2,6 +2,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.lang.Thread;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,57 +16,66 @@ public class Router
 	 *
 	 * @author Jordan Long, Chris Lashley, Jeff Titanich, Kyle Shoop
 	 */
+	public int routeNum = 1;	// 1-4 router id range all must be different
 
-
-    public static void main(String[] args) throws IOException
-    {
-        /**
-         * A server is created listening on port 4446. Each time a new
-         * client connects to the server, a new thread is created
-         * for that specific client.
-         */
+	public static void main(String[] args) throws IOException
+	{
+		/**
+		 * A server is created listening on port 4446. Each time a new
+		 * client connects to the server, a new thread is created
+		 * for that specific client.
+		 */
 
 		@SuppressWarnings("resource") //Server is never closed
 		ServerSocket server = new ServerSocket(4446);
 		Router r = new Router();
-        System.out.println("Router Started: "+ server);
+		System.out.println("Router Started: "+ server);
 
-        while (true)
-        {
-        	Socket socket = server.accept();
-        	RouterHandler rh  = new RouterHandler(socket, r);
-        	rh.start();
-        }
-    }
+		while (true)
+		{
+			Socket socket = server.accept();
+			RouterHandler rh  = new RouterHandler(socket, r);
+			rh.start();
+		}
+	}
 }
 
 class RouterHandler extends Thread
 {
-    private Socket socket;
-    private Router router;
-    DataInputStream input;
-    DataOutputStream output;
-    public RouterHandler(Socket socket, Router r) throws IOException
-    {
-        this.socket = socket;
-        this.router = r;
-        input = new DataInputStream(socket.getInputStream());
-        output = new DataOutputStream(socket.getOutputStream());
+	private static int SPORT = 4446;	// server port (to receive from client)
+	private static int CPORT = 4447;	// client port (to send to client)
+	private static Socket socket;
+	private Router router;
+	private int routeId = 1;	// 1-4 router hop id number, each must be different
+	DataInputStream input;
+	DataOutputStream output;
+	public RouterHandler(Socket socket, Router r) throws IOException
+	{
+		this.socket = socket;
+		this.router = r;
+		input = new DataInputStream(socket.getInputStream());
+		output = new DataOutputStream(socket.getOutputStream());
 
-    }
+	}
 
 
-    /**
-     * Here is where we read in a packet, print to console, and then forward to another router.
-     */
-    public void run()
-    {
-    	while (socket.isConnected() && !socket.isClosed())
-    	{
-    		byte[] message = receiveMessage();
-    	}
-
-    }
+	/**
+	 * Here is where we read in a packet, print to console, and then forward to another router.
+	 */
+	public void run()
+	{
+		while (socket.isConnected() && !socket.isClosed())
+		{
+			try
+			{
+				receiveMessage();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
 
 
 	/**
@@ -83,11 +93,11 @@ class RouterHandler extends Thread
 
 	/**
 	 *  Read message from Client. Prints to console for reference.
-	 *  @return message The received message from a client.
+	 * @throws IOException
 	 */
 
-    public byte[] receiveMessage()
-    {
+	public void receiveMessage() throws IOException
+	{
 		byte[] message= new byte[5];
 		try
 		{
@@ -97,163 +107,191 @@ class RouterHandler extends Thread
 		{
 			e1.printStackTrace();
 		}
-        byte calculateChecksum = checksum(message);
-        if(calculateChecksum == message[2])
-        {
-        	System.out.println("Client Received Message from Source: " + message[0]);
-        	System.out.println("Checksum valid, message is not corrupt.");
-    		System.out.println("Data1: " + message[3]);
-    		System.out.println("Data2: " + message[4]);
+		byte calculateChecksum = checksum(message);
+		if(calculateChecksum == message[2])
+		{
+			System.out.println("Router Received Message from Source: " + message[0]);
+			System.out.println("Checksum valid, message is not corrupt.");
+			System.out.println("Data1: " + message[3]);
+			System.out.println("Data2: " + message[4]);
 
-    		//Send message to another client or router
-    		sendMessage(message);
-        }
-        else
-        {
-        	System.out.println("Checksum not vaild, message may be corrupt.");
-        }
+			//Send message to another client or router
+			sendMessage(message);
+		}
+		else
+		{
+			System.out.println("Checksum not vaild, message may be corrupt.");
+		}
+	}
 
-		return message;
-    }
+	/**
+	 *  Forward a message to another client.
+	 * @throws IOException
+	 */
 
+	public void sendMessage(byte[] message) throws IOException
+	{
+		int destination = (int) message[1];
+		int nextHop = routingTable(message);
 
+		if(destination == nextHop)
+		{
+			//send to client
+			forwardMessage((int) message[0], (int) message[1], (int) message[3], (int) message[4], SPORT, nextHop, true);
+		}
+		else
+		{
+			//send to router
+			forwardMessage((int) message[0], (int) message[1], (int) message[3], (int) message[4], CPORT, nextHop, false);
+		}
 
-
-    /**
-     *  Forward a message to another client.
-     */
-
-    public void sendMessage(byte[] message)
-    {
-        int destination = message[1];
-        int currentRouter = 0; //not true
-
-    	if(destination == currentRouter)
-        {
-        	//send to client
-        }
-    	else
-    	{
-    		//send to router
-    	}
-
-
-
-    	try
-        {
+		try
+		{
 			output.write(message);
 			output.flush();
 		}
-        catch (IOException e)
-        {
+		catch (IOException e)
+		{
 			e.printStackTrace();
-        }
+		}
 
-    }
+	}
 
+	/**
+	 * Forwards message.
+	 * @param source Source Client ID
+	 * @param destination Destination Client ID
+	 * @param data1 Data packet to be sent
+	 * @param data2 Data packet to be sent
+	 * @param port the port the message needs to be sent via
+	 * @param hop the router for the msg destination
+	 * @param client boolean that is true if the current router is for the destination client, false if needs to be routed
+	 * @throws IOException
+	 */
+	public static void forwardMessage(int source, int destination, int data1, int data2, int port, int hop, boolean client) throws IOException
+	{
+		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		byte[] message = {(byte) (source), (byte) (destination), 0, (byte) (data1), (byte) (data2)};
+		message[2] = checksum(message);		//Calculate checksum and attach to message.
 
+		if (client)
+		{
+			// needs to go to client on different port
+			InetAddress host = InetAddress.getLocalHost();
+			Socket destSocket = new Socket(host.getHostName(), SPORT);
+			output = new DataOutputStream(destSocket.getOutputStream());
+			System.out.println("Router forwarded message to Destination: " + message[1]);
+		}
+		else
+		{
+			System.out.println("Router forwarded message to Router: " + hop);
+		}
+		System.out.println("Data1: " + message[3]);
+		System.out.println("Data2: " + message[4]);
+		System.out.println("");
 
+		output.write(message);
+		output.flush();
+	}
 
+	/**
+	 * Routing table for messages to be sent.
+	 * @param message
+	 * @return router the correct router.
+	 */
+	public int routingTable(byte[] message)
+	{
+		int source = message[0];
+		int destination = message[1];
+		int route = 0;
 
-
-    /**
-     * Routing table for messages to be sent.
-     * @param message
-     * @return router the correct router.
-     */
-    public int routingTable(byte[] message)
-    {
-    	int source = message[0];
-    	int destination= message[1];
-    	int route = 0;
-
-    	//Router 1
-    	if(source == 1)
-    	{
-        	if(destination == 1)
-        	{
-        		route = 1;
-        	}
-        	else if(destination == 2)
-        	{
-        		route = 2;
-        	}
-        	else if(destination == 3)
-        	{
-
-        	}
-        	else if(destination == 4)
-        	{
-
-        	}
-    	}
-    	//Router 2
-    	else if(source == 2)
-    	{
-        	if(destination == 1)
-        	{
-
-        	}
-        	else if(destination == 2)
-        	{
-
-        	}
-        	else if(destination == 3)
-        	{
-
-        	}
-        	else if(destination == 4)
-        	{
-
-        	}
-    	}
-
-    	//Router 3
-    	else if(source == 3)
-    	{
-        	if(destination == 1)
-        	{
-
-        	}
-        	else if(destination == 2)
-        	{
-
-        	}
-        	else if(destination == 3)
-        	{
-
-        	}
-        	else if(destination == 4)
-        	{
-
-        	}
-    	}
-    	//Router 4
-    	else if(source == 4)
-    	{
-        	if(destination == 1)
-        	{
-
-        	}
-        	else if(destination == 2)
-        	{
-
-        	}
-        	else if(destination == 3)
-        	{
-
-        	}
-        	else if(destination == 4)
-        	{
-
-        	}
-    	}
-
-
-    	return route;
-
-    }
-
-
-
+		// if this is destination router no need to use table
+		if (routeId == destination)
+			return routeId;
+		else
+		{
+			// use table to find which router to send to get msg to destination
+			//Router 1
+			if(source == 1)
+			{
+				if(destination == 1)
+				{
+					route = 1;
+				}
+				else if(destination == 2)
+				{
+					route = 2;
+				}
+				else if(destination == 3)
+				{
+					route = 2;
+				}
+				else if(destination == 4)
+				{
+					route = 4;
+				}
+			}
+			//Router 2
+			else if(source == 2)
+			{
+				if(destination == 1)
+				{
+					route = 1;
+				}
+				else if(destination == 2)
+				{
+					route = 2;
+				}
+				else if(destination == 3)
+				{
+					route = 3;
+				}
+				else if(destination == 4)
+				{
+					route = 3;
+				}
+			}
+			//Router 3
+			else if(source == 3)
+			{
+				if(destination == 1)
+				{
+					route = 4;
+				}
+				else if(destination == 2)
+				{
+					route = 2;
+				}
+				else if(destination == 3)
+				{
+					route = 3;
+				}
+				else if(destination == 4)
+				{
+					route = 4;
+				}
+			}
+			//Router 4
+			else if(source == 4)
+			{
+				if(destination == 1)
+				{
+					route = 1;
+				}
+				else if(destination == 2)
+				{
+					route = 1;
+				}
+				else if(destination == 3)
+				{
+					route = 3;
+				}
+				else if(destination == 4)
+				{
+					route = 4;
+				}
+			}
+		}
+		return route;
+	}
 }
